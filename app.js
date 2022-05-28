c2fe3375e0f9e3faa809f3c6523cdb8b989bc9f2
Arterixs <artemontugg@gmail.com>
2022-05-28 19:30:57 +0300

    refactor: 1.2 Alternative for multimedia

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+
+    deleteButtonImg.setAttribute("alt", "icon")
     label.innerText=taskString;
     label.className='task';
 
